3595afc23b3a346b9dc72c266f338f92308878c1
林鑫佑 <davidlin@ryzo.io>
2023-12-08 16:53:14 +0800

    try

--- day2.js
+++ day2.js
@@ -8,18 +8,17 @@ a.split('\n').reduce((pre, cur, curI) => {
     const patternR = /\b(\d+) red\b/g;
     const patternB = /\b(\d+) blue\b/g;
     let isPass = true;
-    const setList = cur.split(';');
+    const setList = cur.substring(8).split(';');
     for (let i = 0; i < setList.length - 1; i++) {
         let set = setList[i];
         const G = parseInt(set.match(patternG)) || 0;
         const R = parseInt(set.match(patternR)) || 0;
         const B = parseInt(set.match(patternB)) || 0;
-        console.log(R,G,B)
-        isPass = G <= 13 && R <= 12 && B <= 14;
+        isPass = G >= 13 && R >= 12 && B >= 14;
         if (!isPass) {
             break;
         }
     }
-    console.log('isPass:', isPass, 'index;', curI)
-    return (isPass) ? curI + pre : pre
+    console.log('isPass:', isPass, 'index;', curI+1, 'total:', (isPass) ? curI + pre : pre)
+    return (isPass) ? curI + 1 + pre : pre
 }, 0)
