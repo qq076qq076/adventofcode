0b240075ac9c59a8c140df2af33d5fa2f6fd0cd3
David Lin <qq076qq076@gmail.com>
2023-12-09 23:45:55 +0800

    finish day2 part1

--- day2.js
+++ day2.js
@@ -9,16 +9,16 @@ a.split('\n').reduce((pre, cur, curI) => {
     const patternB = /\b(\d+) blue\b/g;
     let isPass = true;
     const setList = cur.substring(8).split(';');
-    for (let i = 0; i < setList.length - 1; i++) {
+    for (let i = 0; i < setList.length; i++) {
         let set = setList[i];
         const G = parseInt(set.match(patternG)) || 0;
         const R = parseInt(set.match(patternR)) || 0;
         const B = parseInt(set.match(patternB)) || 0;
-        isPass = G >= 13 && R >= 12 && B >= 14;
+        isPass = G <= 13 && R <= 12 && B <= 14;
         if (!isPass) {
             break;
         }
     }
-    console.log('isPass:', isPass, 'index;', curI+1, 'total:', (isPass) ? curI + pre : pre)
+    console.log('isPass:', isPass, 'Game;', curI + 1, 'total:', (isPass) ? curI + 1 + pre : pre)
     return (isPass) ? curI + 1 + pre : pre
 }, 0)
